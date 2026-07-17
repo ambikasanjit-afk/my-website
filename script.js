de5b01670150a6a279df490c08fe55451b8635c0
ambikasanjit-afk <ambikasanjit@gmail.com>
2026-07-17 13:40:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function searchTools() {
-    alert("Search feature coming soon!");
+    let input = document.getElementById("search").value.toLowerCase();
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card) {
+        let text = card.innerText.toLowerCase();
+
+        if (text.includes(input)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
 }
