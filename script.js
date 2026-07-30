c4eb75c6f786a923d87f8f13bbe27674bd318c0f
ambikasanjit-afk <ambikasanjit@gmail.com>
2026-07-31 00:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,19 @@ window.addEventListener("load", function () {
     document.getElementById("loader").style.display = "none";
   }, 1500);
 });
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
